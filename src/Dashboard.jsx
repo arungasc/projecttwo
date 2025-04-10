ce429920e1ea702c70828f8b75501bfee5e58330
arungasc <arungasc2@gmail.com>
2025-04-10 00:56:22 -0700

    Update Dashboard.jsx

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Dashboard = () => {
+  const name = localStorage.getItem("username");  // 🔁 Get name from storage
+
   return (
     <div className="text-center mt-5 mb-5">
-      <h1>🎉 Welcome to you!</h1>
+      <h1>🎉 Welcome Mr/Mrs. {name}!</h1>
     </div>
   );
 };
